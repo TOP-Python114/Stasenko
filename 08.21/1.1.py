32ce7b023f69fe7ad38f697d9a40e2a38e671ebd
Citxr <pechuha2005@gmail.com>
2022-09-11 12:51:03 +0300

    Update 1.1.py

--- 08.21/1.1.py
+++ 08.21/1.1.py
@@ -1,16 +1,41 @@
 from random import choice
 
 # 1)
-suit = ['Бубны', 'Пики', 'Черви', 'Крести']
+suit1 = ['Бубны', 'Пики', 'Черви', 'Крести']
 
+loc1 = []
+iter_list = iter(loc1)
 
-list = []
-iter_list = iter(list)
-for j in suit:
+for count in suit1:
     for i in range(1, 14):
-        list.append([i, j])
+        loc1.append([i, count])
         print(iter_list.__next__())
+
+# Я попробовал сделать через enumerate, но код был слишком громосткий и не особо отличался от прошлого
+
 
 # 2)
 
-print(choice(list))
+
+loc2 = []
+def random_suit(cards):
+    while cards:
+        rand_card = choice(cards)
+        loc2.append(rand_card)
+        loc1.remove(rand_card)
+        if len(loc2) == 52:
+            return loc2
+
+random_set = random_suit(loc1)
+
+for i in random_set:
+    print(i, end=' --> ')
+
+
+
+
+
+
+
+
+
