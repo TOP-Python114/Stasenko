b9e5541d411ffacdc3fe498883fe65f010655356
Gennadiy <renardes@gmail.com>
2022-09-15 11:02:15 +0500

    домашнее задание от 09.11

--- 09.04/1.1.py
+++ 09.04/1.1.py
@@ -1,20 +1,32 @@
+# ДОБАВИТЬ: строку документации для класса
 class ClassBuilder:
     def __init__(self, class_name: str):
         self.class_name = class_name
         self.lines: list[str] = []
 
-    def add_lines(self, *line: str|int|bool):
+    # ИСПРАВИТЬ: вы добавляете атрибут, почему у вас имя add_lines (да ещё во множественном числе) — может имя add_attribute будет больше соответствовать назначению метода?
+    # ДОБАВИТЬ: строку документации для метода — начинается с глагола и отвечает на вопрос "что делает?"
+    def add_lines(self,
+                  # ОТВЕТИТЬ: а почему произвольное количество аргументов? в самом общем случае там может быть имя атрибута, аннотация типа и значение по умолчанию — три элемента максимум; может, имеет смысл их явно перечислить в этом методе?
+                  *line: str | int | bool):
         self.lines += [line]
         return self
 
+    # ДОБАВИТЬ: строку документации для метода
     def clear(self):
         self.lines.clear()
 
     def __str__(self):
         if not self.lines:
+            # ИСПРАВИТЬ: ключевое слово class пишется в нижнем регистре
             return f"Class {self.class_name}:\n\tpass"
 
+        # ИСПРАВИТЬ: имена функций в Python мы пишем в нижнем регистре, разделяя слова в имени символом подчёркивания — это называется змеиный_нижний_регистр (snake_lower_case)
+        # ИСПРАВИТЬ: как имя meaning ("значение") соотносится с назначением этой функции (возврат имени класса)?
+        # ДОБАВИТЬ: строку документации для функции
         def Meaning(line):
+            # ИСПОЛЬЗОВАТЬ: а ещё можно так:
+            # return type(line).__name__
             if type(line) == int:
                 return 'int'
             if type(line) == str:
@@ -23,13 +35,16 @@ class ClassBuilder:
                 return 'bool'
 
         s_line = ", ".join(
+            # ДОБАВИТЬ: значения по умолчанию для ваших атрибутов
             line[0] + f': {Meaning(line[1])}'
             for line in self.lines
         )
         finished_lines = '\n\t\t'.join(
+            # ИСПРАВИТЬ: преобразовывать в список не нужно, метод join() принимает и генераторные выражения тоже
             [f"self.{line[0]} = " + str(line[1])
              for line in self.lines]
         )
+        # ИСПРАВИТЬ: ключевое слово class пишется в нижнем регистре
         return f"Class {self.class_name}:\n" \
                f"\tdef __init__(self, {s_line}):\n" \
                f"\t\t{finished_lines}"
